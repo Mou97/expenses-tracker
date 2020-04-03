fff4e0126eaf9f0185f037392357706e78c2d4f8
Mou97 <m.taguiouine@esi-sba.dz>
2020-04-03 18:18:32 +0100

    add input to filter expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters'
 
 export const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
+
     </div>
 )
 
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setTextFilter } from '../actions/filters'
 
-export default () => {
+
+
+const ExpenseListFilters = (props) => {
     return (
         <div>
-            <input type="text" />
+            <input type="text" defaultValue={props.filters.text} onChange={e => {
+                props.dispatch(setTextFilter(e.target.value))
+            }} />
         </div>
     )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        filters: state.filters
+    }
+}
+
+export default connect(mapStateToProps)(ExpenseListFilters)
