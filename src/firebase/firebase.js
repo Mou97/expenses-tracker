03fe0a4f5967c381fff80a178aea68bf9ff82486
Mou97 <m.taguiouine@esi-sba.dz>
2020-04-23 14:18:45 +0100

    update .gitignorefiel

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,7 @@ import * as firebase from 'firebase'
 
 const config = {
 
-    apiKey: "AIzaSyCyRx3MsynbZW67NT6wh6LkZt2UakKdJnk",
-    authDomain: "expensify-alpha-app.firebaseapp.com",
-    databaseURL: "https://expensify-alpha-app.firebaseio.com",
-    projectId: "expensify-alpha-app",
-    storageBucket: "expensify-alpha-app.appspot.com",
-    messagingSenderId: "896235756459",
-    appId: "1:896235756459:web:12c00bc60142f2df5d90ee"
+
 }
 
 firebase.initializeApp(config)
