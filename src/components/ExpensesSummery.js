d1b2875f6469ef2f42fc3d3fece1bbcc2cb47ac3
Mou97 <m.taguiouine@esi-sba.dz>
2020-06-12 15:32:56 +0100

    style summery section

--- src/components/ExpensesSummery.js
+++ src/components/ExpensesSummery.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux'
 import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expense-total'
+import { Link } from 'react-router-dom'
 
 const ExpensesSummery = ({ expenseCount, expensesTotal }) => {
 
     const expenseWord = expenseCount === 1 ? 'expense' : "expenses"
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} | totalling {formattedExpensesTotal} </h1>
+        <div className='page-header'>
+            <div className='content-container'>
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord}, totalling <span>{formattedExpensesTotal}</span> </h1>
+                <div className='page-header__action'>
+                    <Link className='button' to='/create'>Add Expense</Link>
+                </div>
+            </div>
         </div>
     )
 
